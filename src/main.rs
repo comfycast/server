3ec1bcccec459a488b31678cdd42aabfefba1873
Federico Maria Morrone <contact@morrone.dev>
2024-11-01 18:33:25 +0100

    Add basic http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use axum::{routing::get, Router};
+use tracing::info;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8008").await?;
+    info!("Started server on port 8008");
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
