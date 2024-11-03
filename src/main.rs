34181c40c0581d62da496ca274c47b423f1f7e1e
Federico Maria Morrone <contact@morrone.dev>
2024-11-03 19:32:40 +0100

    Mock some routes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use anyhow::Result;
-use axum::{extract::Path, routing::get, Json, Router};
+use axum::{
+    extract::Path,
+    routing::{get, post},
+    Json, Router,
+};
 use serde::{Deserialize, Serialize};
 use tower_http::cors::{Any, CorsLayer};
 use tracing::info;
@@ -10,6 +14,8 @@ async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
     let app = Router::new()
+        .route("/videos", get(videos))
+        .route("/stream/create/:video", get(create_stream))
         .route("/stream/:id", get(stream))
         .layer(CorsLayer::new().allow_origin(Any).allow_methods(Any));
 
@@ -17,6 +23,34 @@ async fn main() -> Result<()> {
     info!("Started server on port 8008");
     axum::serve(listener, app).await?;
     Ok(())
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Video {
+    id: String,
+}
+
+async fn videos() -> Json<Vec<Video>> {
+    let num = 10usize;
+    let mut list = Vec::with_capacity(10);
+
+    for e in 0..num {
+        list.push(Video {
+            id: format!("real-id-{e}"),
+        });
+    }
+
+    Json(list)
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct CreateResponse {
+    id: Uuid,
+}
+
+async fn create_stream(Path(video): Path<String>) -> Json<CreateResponse> {
+    info!("Creating stream for video {video}");
+    Json(CreateResponse { id: Uuid::new_v4() })
 }
 
 #[derive(Debug, Deserialize, Serialize)]
